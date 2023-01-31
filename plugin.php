31b2fc3a92853c03cc22f6eeef89afe2e5df0b13
dekode-this <daniel@dekode.no>
2023-01-31 09:40:03 +0100

    Added number of posts and displayFeaturedImages attributes to block.json Added render_callback into the array of options in thes blocks_course_latest_posts_block_init() function Created the blocks_course_render_latest_posts_block callback function which is the function that displays the PHP Added  as the functions main arguments which gives me access to the attributes from block.json In the PHP function blocks_course_render_latest_posts_block() I created an  variable containing posts_per_page and post_status protperties posts_per_page uses the numberOfPosts attribute from block.json, post_status is hard coded to only display published posts I created  variable that uses get_posts() function which is passed posts_per_pahe and post_status via the  variable The blocks_course_render_latest_posts_block() callback function returns the  variable, this  variable is what renders the <ul> get_block_wrapper_attributes() spreads the props and css classes to the <ul> wrapper. Set a  var that uses get_the_title() on the next line I rewrite this variable with an if statement if the post doesn't have a title, it will display (no title). Created permalink and excerpt variables and created the markup using .=

--- plugin.php
+++ plugin.php
@@ -20,7 +20,44 @@
  *
  * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
  */
+
+function blocks_course_render_latest_posts_block($attributes) {
+	$args = array (
+		'posts_per_page' => $attributes['numberOfPosts'], // will access the numberOfPosts attribute from the block.json file
+		'post_status' => 'publish', // will only show published posts
+	);
+	$recent_posts = get_posts($args);
+
+	//var_dump($recent_posts); // this will render the array of posts to the screen.
+	
+	$posts = '<ul ' . get_block_wrapper_attributes() .'>';
+	foreach ($recent_posts as $post) { // will loop through the array of posts and display them in a list. See https://www.w3schools.com/php/php_looping_foreach.asp
+		$title = get_the_title($post); // will get the title of the post
+		$title = $title ? $title : __('(No title)', 'latest-posts'); // if the post doesn't have a title, it will display (no title)
+		$permalink = get_permalink($post); // will get the permalink of the post
+		$excerpt = get_the_excerpt($post); // will get the excerpt of the post // $post is an object, so we can access the properties of the object
+		//$posts .= '<li><a href="' . get_permalink($post->ID) . '">' . $post->post_title . '</a></li>';
+		$posts .= '<li>';
+		$posts .= '<h5><a href="' . esc_url($permalink) . '">' . $title . '</a></h5>';
+		$posts .= '<time datetime="' . esc_attr(get_the_date('c', $post)) . '">' . esc_html(get_the_date('', $post)) . '</time>'; // will get the date of the post // passing an empty string to get_the_date() will return the date in the format set in the WordPress settings
+		if(!empty($excerpt)) { // if the post has an excerpt, it will display it
+			$posts .= '<p>' . $excerpt . '</p>'; // will display the excerpt of the post
+		}
+		$posts .= '</li>';
+	}
+	$posts .= '</ul>';
+
+	// in the above code, between lines 31 and 35 explain to me why we have to decalre $posts on multiple lines?
+	// why can't we just do $posts = '<ul ' . get_block_wrapper_attributes() .'><li><a href="' . get_permalink($post->ID) . '">' . $post->post_title . '</a></li></ul>';?
+	//  I'm guessing it's because we want to be able to add more posts to the list, but I'm not sure why we would want to do that.
+	// answer: we want to be able to add more posts to the list, but we also want to be able to add more attributes to the list, like the class name, etc.
+
+	return $posts;
+}
+
 function blocks_course_latest_posts_block_init() {
-	register_block_type_from_metadata( __DIR__ );
+	register_block_type_from_metadata( __DIR__, array(
+		'render_callback' => 'blocks_course_render_latest_posts_block',
+	));
 }
 add_action( 'init', 'blocks_course_latest_posts_block_init' );
